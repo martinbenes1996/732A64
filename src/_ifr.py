d087c3f8910b64f484fe30374cdb7156c9b6df1b
Martin Benes <martinbenes1996@gmail.com>
2021-05-02 18:49:31 +0200

    ifr editted

--- src/_ifr.py
+++ src/_ifr.py
@@ -1,23 +1,69 @@
+# -*- coding: utf-8 -*-
+"""IFR internal module.
+
+Module containing operation for IFR (infection fatality ratio).
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import uniform,bernoulli
 
-def rvs(a = .004, b = .01, size = 10000):
-    # create draws
-    draws = uniform.rvs(a, b - a, size = size, random_state=12345)
-    return draws
-def pdf(a = .004, b = .01, size = 10000):
-    # create draws
+def rvs(size=1e4, a=.004, b=.01):
+    """Draw a random sample of IFR.
+    
+    Args:
+        size (int): Number of samples.
+        a,b (float): Parameters of the distribution U(a,b).
+    Returns:
+        np.array: IFR sample.
+    """
+    sample = uniform.rvs(a, b - a, size = size, random_state=12345)
+    return sample
+
+def pdf(size=1e4, a=.004, b=.01):
+    """Make a grid of IFR for plotting.
+    
+    Args:
+        size (int): Number of values in grid.
+        a,b (float): Parameters of the distribution U(a,b).
+    Returns:
+        np.array: Grid of x-axis.
+        np.array: Values of U(a,b) PDF for the grid.
+    """
     xgrid = np.linspace(a - (b-a)/4, b + (b-a)/4, num=size)
     fx = uniform.pdf(xgrid, a, b - a)
-    #draws = uniform.rvs(a, b - a, size = size, random_state=12345)
-    #draws = bernoulli.rvs(prior_draw, size = size, random_state=12345)
     return xgrid, fx
 
 def plot_ifr(save = False, name = 'img/sir/ifr.png'):
     # simulate
-    draws = rvs(size = 10000)
+    draws = rvs()
     # get density
     xgrid, fx = pdf()
     # plot
